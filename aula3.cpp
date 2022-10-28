375b30040c1ccc995c26f5a767e9b11b73d2ea5e
lukecampardo <lucascampardo@outlook.com>
2022-10-28 14:25:18 -0300

    Primeiro commit enquanto já estava na Aula 04

---
+++ aula3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){	
+	
+	cout << "Hello, World!\n";
+	cout << "Foque no C++ e absorva tudo o que voce puder dele." << endl;
+	
+	system("pause");
+	return 0;
+}
---
+++ aula4.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	
+	// TIPO NOME;
+	// TIPO NOME = VALOR;
+	
+	int vidas=0; // 10, 25, 83
+	char letra='L'; // 'L'
+	double decimal=5.8; // 5.8888888
+	float decimal2=3.6; // 3,6
+	bool vivo=true; // true = verdadeiro & false = falso / true = 1 & false = 0
+	string nome="Lucas"; // "Lucas"
+	
+	cout << vidas << "\n";
+	cout << letra << "\n";
+	cout << decimal << "\n";
+	cout << vivo << "\n";
+	cout << nome << "\n";
+		
+	return 0;
+}
