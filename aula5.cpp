4e7f7e02b3a87b1d332bee1ca3d7a9f2c4da3061
lukecampardo <lucascampardo@outlook.com>
2022-10-29 14:56:18 -0300

    Aula 5 onde foi falado do uso de variáveis múltiplas e constantes

---
+++ aula5.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+#define pi 3.1415
+#define name cout << "Lucas";
+
+int main() {
+	
+	int vidas=1, livros=30, life=100;
+	
+	cout << pi << "\n\n";
+	name
+	
+	// int vidas;
+	// int livros;
+	// int life;
+	// int vidas; int livros; int life;
+	
+	return 0;
+}
